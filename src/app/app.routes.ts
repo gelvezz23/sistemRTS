650fcc1c97601cf0f847cd7f10e7503dd9be5ac9
gelvezz23 <mao_23_@hotmail.com>
2024-04-23 08:53:22 -0500

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'form', component: FormularioComponent },
+];
