4fa271606948c1c016ad086d161179808dcbbb34
gelvezz23 <mao_23_@hotmail.com>
2024-04-23 21:11:12 -0500

    update form

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -8,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrl: './formulario.component.scss',
 })
 export class FormularioComponent {
-  task = ['asadjwdf', 'asdsa', 'aaaaa'];
+  public questions: { id: number; question: string; value: string }[];
+  constructor() {
+    this.questions = [
+      {
+        id: 1,
+        question:
+          '¿Coresponde una actividad de restaurante, cafeterías, autoservicio, bares y similares?',
+        value: '',
+      },
+      { id: 2, question: '¿Es realizada con una franquicia?', value: '' },
+      { id: 3, question: '¿Se utilizan marcas propias?', value: '' },
+      { id: 4, question: '¿Está gravada con IVA?', value: '' },
+      {
+        id: 5,
+        question: '¿Consiste en venta de bienes corporales muebles?',
+        value: '',
+      },
+      { id: 6, question: '¿Estos bienes están excluidos?', value: '' },
+      {
+        id: 7,
+        question:
+          '¿Consiste en la prestación de servicios en el territorio nacional o desde el exterior?',
+        value: '',
+      },
+      { id: 8, question: '¿Estos servicios están excluidos?', value: '' },
+    ];
+  }
+
+  public getAnswer(value: string, id: number): void {
+    const questionIndex = this.questions.findIndex(
+      (question) => question.id === id
+    );
+
+    if (questionIndex !== -1) {
+      this.questions[questionIndex].value = value;
+    }
+    localStorage.setItem('response', JSON.stringify(this.questions));
+  }
 }
