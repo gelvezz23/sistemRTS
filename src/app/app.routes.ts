63ddfeaccf19a7c26cefff7bcc0651c464cf0258
gelvezz23 <mao_23_@hotmail.com>
2024-04-30 23:57:48 -0500

    formulario 3 add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FormularioComponent } from './pages/formulario/formulario.component';
 import { FormularioDosComponent } from './pages/formulario-dos/formulario-dos.component';
+import { FormularioTresComponent } from './pages/formulario-tres/formulario-tres.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'form', component: FormularioComponent },
   { path: 'form2', component: FormularioDosComponent },
+  { path: 'form3', component: FormularioTresComponent },
 ];
---
+++ src/app/pages/formulario-tres/formulario-tres.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-tres',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-tres.component.html',
+  styleUrl: './formulario-tres.component.scss'
+})
+export class FormularioTresComponent {
+
+}
