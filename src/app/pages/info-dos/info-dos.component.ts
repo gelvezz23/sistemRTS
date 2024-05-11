fca106c7ea787458b89569003f849d1d99c1d2b1
gelvezz23 <mao_23_@hotmail.com>
2024-05-11 00:14:49 -0500

    update

--- src/app/pages/info-dos/info-dos.component.ts
+++ src/app/pages/info-dos/info-dos.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-info-dos',
   standalone: true,
   imports: [],
   templateUrl: './info-dos.component.html',
-  styleUrl: './info-dos.component.scss'
+  styleUrl: './info-dos.component.scss',
 })
 export class InfoDosComponent {
-
+  constructor(private router: Router) {}
+  public handleClick() {
+    this.router.navigate(['form']);
+  }
 }
