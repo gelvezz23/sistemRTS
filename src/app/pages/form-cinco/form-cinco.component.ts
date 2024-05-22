e65860f1d204afc42e6ad52279d4b57a099112a5
gelvezz23 <mao_23_@hotmail.com>
2024-05-22 17:23:28 -0500

    feat: update conditions

--- src/app/pages/form-cinco/form-cinco.component.ts
+++ src/app/pages/form-cinco/form-cinco.component.ts
@@ -42,18 +42,113 @@ export class FormCincoComponent {
   }
 
   public viewButtonSeven() {
-    const data = localStorage.getItem('caracterizacion_de_negocio');
-    const data2 = localStorage.getItem('modelos_hipoteticos');
-    const modeloHipoteticos = JSON.parse(data2 || '')[1].quest;
-    const personaJuridica = JSON.parse(data || '').titular.value;
+    const modeloHipoteticos = JSON.parse(
+      localStorage.getItem('modelos_hipoteticos') || ''
+    );
+    const personaJuridica = JSON.parse(
+      localStorage.getItem('caracterizacion_de_negocio') || ''
+    ).titular.value;
     const answer =
       'Más de 4500 a igual o menos de 30.000 UVT (Más de 4500 a igual o menos de 30.000 UVT (Más de $ 211.792.500 a igual o menos de $ 1.411.950.000)';
+
+    const otherAnswer =
+      'Igual o más de 1400 UVT a menos de 3500 UVT (de $65.891.000 a $164.727.500)';
+
     if (personaJuridica === 'persona juridica') {
       return true;
-    } else if (answer === modeloHipoteticos) {
+    }
+    if (
+      answer === modeloHipoteticos[1].quest &&
+      personaJuridica === 'persona natural'
+    ) {
+      return true;
+    }
+    if (
+      modeloHipoteticos[0].quest === otherAnswer &&
+      personaJuridica === 'persona natural'
+    ) {
       return true;
     }
 
-    return personaJuridica === 'persona juridica';
+    if (
+      modeloHipoteticos[0].quest ===
+        'Igual o más de 3500 UVT a igual o menos de 30.000 UVT (de $164.727.500 a $1.411.950.000)' &&
+      personaJuridica === 'persona natural'
+    ) {
+      return true;
+    }
+
+    if (
+      modeloHipoteticos[1].quest ===
+        'Más de 30.000 UVT (Más de $ 1.411.950.000)' &&
+      personaJuridica === 'persona natural'
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  public viewButtonEigth() {
+    const modeloHipoteticos = JSON.parse(
+      localStorage.getItem('modelos_hipoteticos') || ''
+    );
+    const personaJuridica = JSON.parse(
+      localStorage.getItem('caracterizacion_de_negocio') || ''
+    ).titular.value;
+
+    if (personaJuridica === 'persona juridica') {
+      return true;
+    }
+
+    if (
+      personaJuridica === 'persona natural' &&
+      modeloHipoteticos[0].quest ===
+        'Igual o más de 3500 UVT a igual o menos de 30.000 UVT (de $164.727.500 a $1.411.950.000)'
+    ) {
+      return true;
+    }
+
+    if (
+      modeloHipoteticos[1].quest ===
+        'Más de 30.000 UVT (Más de $ 1.411.950.000)' &&
+      personaJuridica === 'persona natural'
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  public viewButtonNine() {
+    const caracterizacion = JSON.parse(
+      localStorage.getItem('caracterizacion_de_negocio') || ''
+    );
+
+    if (
+      caracterizacion.negocio.value === 'comercial' ||
+      caracterizacion.negocio.value === 'profesion liberal o artistica'
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public viewButtonTen() {
+    const caracterizacion = JSON.parse(
+      localStorage.getItem('caracterizacion_de_negocio') || ''
+    );
+
+    const modeloHipoteticos = JSON.parse(
+      localStorage.getItem('modelos_hipoteticos') || ''
+    );
+
+    if (
+      caracterizacion.negocio.value === 'comercial' &&
+      modeloHipoteticos[0].quest ===
+        'Igual o más de 3500 UVT a igual o menos de 30.000 UVT (de $164.727.500 a $1.411.950.000)'
+    ) {
+      return true;
+    }
+    return false;
   }
 }
