d1ae384725728aceb4e6f363f3f3672396c9ad97
gelvezz23 <mao_23_@hotmail.com>
2024-05-21 20:17:12 -0500

    update hu 20

--- src/app/pages/form-cinco/form-cinco.component.ts
+++ src/app/pages/form-cinco/form-cinco.component.ts
@@ -9,9 +9,41 @@ import { Router } from '@angular/router';
   styleUrl: './form-cinco.component.scss',
 })
 export class FormCincoComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.handleLocalStorage();
+  }
+  isViewButtonFive = true;
+  isPersonaNatural = true;
 
   public handleRedirect() {
     this.router.navigate(['form5']);
+  }
+  public handleLocalStorage() {
+    const data = localStorage.getItem('caracterizacion_de_negocio');
+
+    const personaNatural = JSON.parse(data || '').titular.value;
+    const profesionLiberal = JSON.parse(data || '').negocio.value;
+
+    this.isPersonaNatural =
+      personaNatural !== 'persona natural' &&
+      profesionLiberal !== 'profesion liberal o artistica';
+
+    this.isViewButtonFive = this.viewButtonFive();
+  }
+
+  public viewButtonFive() {
+    const data = localStorage.getItem('modelos_hipoteticos');
+    const quest = JSON.parse(data || '')[0].quest;
+
+    const answer =
+      'Igual o más de 3500 UVT a igual o menos de 30.000 UVT (de $164.727.500 a $1.411.950.000)';
+    const valid = quest === answer;
+    return valid;
+  }
+
+  public viewButtonSeven() {
+    const data = localStorage.getItem('caracterizacion_de_negocio');
+    const personaJuridica = JSON.parse(data || '').titular.value;
+    return personaJuridica === 'persona juridica';
   }
 }
