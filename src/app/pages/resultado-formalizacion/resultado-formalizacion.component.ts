5d2af63113123edd8001020133894c7add9fc9d8
gelvezz23 <mao_23_@hotmail.com>
2024-06-18 10:19:20 -0500

    update hu 9

--- src/app/pages/resultado-formalizacion/resultado-formalizacion.component.ts
+++ src/app/pages/resultado-formalizacion/resultado-formalizacion.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavbarTwoComponent } from '../../components/navbar-two/navbar-two.component';
 @Component({
   selector: 'app-resultado-formalizacion',
   standalone: true,
-  imports: [],
   templateUrl: './resultado-formalizacion.component.html',
   styleUrl: './resultado-formalizacion.component.scss',
+  imports: [NavbarTwoComponent],
 })
 export class ResultadoFormalizacionComponent {
   formalizado = JSON.parse(
@@ -14,7 +15,7 @@ export class ResultadoFormalizacionComponent {
   count = 0;
   public getResultTotal() {
     this.formalizado.forEach((items: { value: string }) => {
-      if (items.value === 'si') {
+      if (items.value === 'si' || items.value === '') {
         this.count++;
       }
     });
