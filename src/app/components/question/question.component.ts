4692c3459ff9d43b6419e5651d8ea11d26a71e55
gelvezz23 <mao_23_@hotmail.com>
2024-06-19 11:05:46 -0500

    feat: update images

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -9,4 +9,26 @@ import { Component, Input } from '@angular/core';
 })
 export class QuestionComponent {
   @Input({ required: true }) quest!: any;
+
+  public viewimage() {
+    if (this.quest.id === 2) {
+      return true;
+    }
+    if (this.quest.id === 3) {
+      return true;
+    }
+    if (this.quest.id === 4) {
+      return true;
+    }
+    if (this.quest.id === 6) {
+      return true;
+    }
+    if (this.quest.id === 7) {
+      return true;
+    }
+    if (this.quest.id === 8) {
+      return true;
+    }
+    return false;
+  }
 }
